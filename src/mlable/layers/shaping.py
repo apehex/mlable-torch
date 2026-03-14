17be82eed24a6e6bdc3071f7c49c50d4d21d898b
apehex <17317183+apehex@users.noreply.github.com>
2026-03-14 15:17:54 +0100

    Migrate the shaping layers from my Keras lib.

--- src/mlable/layers/shaping.py
+++ src/mlable/layers/shaping.py
@@ -3,80 +3,74 @@ import torch.nn
 
 import mlable.shapes
 
-# UTILS ########################################################################
-
-def _normalize_shape(shape: list) -> list:
-    return [-1 if __d is None else __d for __d in shape]
-
-def _normalize_dim(dim: int) -> int:
-    return -1 if (dim is None or dim < 0) else dim
-
-def _multiply_dim(dim_l: int, dim_r: int) -> int:
-    return -1 if (dim_l == -1 or dim_r == -1) else dim_l * dim_r
-
-def _divide_dim(dim_l: int, dim_r: int) -> int:
-    return -1 if (dim_l == -1 or dim_r == -1) else dim_l // dim_r
-
 # DIVIDE #######################################################################
 
 class Divide(torch.nn.Module):
     def __init__(
         self,
-        input_axis: int, # relative to the NEW shape / rank
-        output_axis: int, # same
+        axis: int, # relative to the original shape
         factor: int,
         insert: bool=False,
+        right: bool=True,
         **kwargs
     ) -> None:
         super(Divide, self).__init__(**kwargs)
-        self._input_axis = input_axis
-        self._output_axis = output_axis
-        self._factor = factor
-        self._insert = insert
+        # save for import, export, duplication, etc
+        self._config = {
+            'axis': axis,
+            'factor': factor,
+            'insert': insert,
+            'right': right,}
 
-    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        # infer the dimension of the symbolic axis
-        __shape = _normalize_shape(list(inputs.shape))
-        # rank, according to the new shape
-        __rank = len(__shape) + int(self._insert)
-        # axes, taken from the new shape
-        __axis0 = self._input_axis % __rank
-        __axis1 = self._output_axis % __rank
-        # option to group data on a new axis
-        if self._insert: __shape.insert(__axis1, 1)
-        # move data from axis 0 to axis 1
-        __shape[__axis0] = _divide_dim(__shape[__axis0], self._factor)
-        __shape[__axis1] = _multiply_dim(__shape[__axis1], self._factor)
-        return inputs.view(*__shape) #.squeeze(1)
+    def call(self, inputs: torch.Tensor, **kwargs) -> torch.Tensor:
+        return mlable.shaping.axes.divide(data=inputs, **self._config)
 
 # MERGE ########################################################################
 
 class Merge(torch.nn.Module):
     def __init__(
         self,
-        left_axis: int=-2,
-        right_axis: int=-1,
-        left: bool=True,
+        axis: int,
+        right: bool=True,
         **kwargs
     ) -> None:
         super(Merge, self).__init__(**kwargs)
-        self._left_axis = left_axis
-        self._right_axis = right_axis
-        self._left = left
+        # save for import / export
+        self._config = {
+            'axis': axis,
+            'right': right,}
 
-    def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        # infer the dimension of the symbolic axis
-        __shape = _normalize_shape(list(inputs.shape))
-        __rank = len(__shape)
-        # target axes
-        __axis_l = self._left_axis % __rank
-        __axis_r = self._right_axis % __rank
-        # new axis
-        __dim = _multiply_dim(__shape[__axis_l], __shape[__axis_r])
-        __axis_k = __axis_l if self._left else __axis_r # kept axis
-        __axis_d = __axis_r if self._left else __axis_l # deleted axis
-        # new shape
-        __shape[__axis_k] = __dim
-        __shape.pop(__axis_d)
-        # actually merge the two axes
-        return inputs.view(*__shape)
+    def call(self, inputs: torch.Tensor, **kwargs) -> torch.Tensor:
+        return mlable.shaping.axes.merge(data=inputs, **self._config)
+
+# SWAP #########################################################################
+
+class Swap(torch.nn.Module):
+    def __init__(
+        self,
+        left_axis: int,
+        right_axis: int,
+        **kwargs
+    ) -> None:
+        super(Swap, self).__init__(**kwargs)
+        # save for import / export
+        self._config = {'left_axis': left_axis, 'right_axis': right_axis,}
+
+    def call(self, inputs: torch.Tensor, **kwargs) -> torch.Tensor:
+        return mlable.shaping.axes.swap(inputs, **self._config)
+
+# MOVE #########################################################################
+
+class Move(torch.nn.Module):
+    def __init__(
+        self,
+        from_axis: int,
+        to_axis: int,
+        **kwargs
+    ) -> None:
+        super(Move, self).__init__(**kwargs)
+        # save for import / export
+        self._config = {'from_axis': from_axis, 'to_axis': to_axis,}
+
+    def call(self, inputs: torch.Tensor, **kwargs) -> torch.Tensor:
+        return mlable.shaping.axes.move(inputs, **self._config)
