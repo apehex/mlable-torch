9ee9a5fa3aceab9b25cfdd912bf5d584c1ac5726
apehex <17317183+apehex@users.noreply.github.com>
2026-04-17 19:03:44 +0200

    Move the helper to free memory here.

--- src/mlable/models.py
+++ src/mlable/models.py
@@ -1,3 +1,6 @@
+import gc
+
+import torch.cuda
 import torch.nn
 
 # GENERIC ######################################################################
@@ -6,3 +9,18 @@ def freeze(model: torch.nn.Module) -> None:
     """Disable gradients for all the parameters of a given model."""
     for __p in model.parameters():
         __p.requires_grad_(False)
+
+# MEMORY #######################################################################
+
+def free_memory(
+    model: object=None
+) -> None:
+    # drop references
+    if model is not None:
+        del model
+    # run garbage collection
+    gc.collect()
+    # free CUDA memory
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+        torch.cuda.ipc_collect()
